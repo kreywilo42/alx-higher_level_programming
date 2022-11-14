e949544096f73964f071c82255ba0a8b893edb93
unknown <kreywilo42@gmail.com>
2022-11-14 15:48:01 +0300

    'webscrapping'

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode);
+});
+
---
+++ 0x15-javascript-web_jquery/0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+JS readme file
